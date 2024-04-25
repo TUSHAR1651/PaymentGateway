be90c0c371c0d4c84ec66564a58a47a090a93a67
Tushar Khurana <tusharkhurana526@gmail.com>
2024-04-26 02:27:12 +0530

    first commit

---
+++ src/main/java/com/example/patmentgateway/PatmentGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.patmentgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatmentGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatmentGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/patmentgateway/PatmentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patmentgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatmentGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
